be7bc4862f3595f524f06cf320dee36525135d3d
Charlie <charlie@lawsnote.com>
2022-06-23 16:40:50 +0800

    [feat] src/index basic content

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from '@Components/App'
 import ErrorBoundary from '@Components/Common/ErrorBoundary'
-
-import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
